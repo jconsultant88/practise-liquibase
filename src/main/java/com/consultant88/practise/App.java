8976983832f605bebf4d5ca49b75263296370b92
Venugopal Vemula <Venugopal.Vemula@monitise.com>
2015-06-18 15:55:06 +0100

    first commit

---
+++ src/main/java/com/consultant88/practise/App.java
@@ -0,0 +1,13 @@
+package com.consultant88.practise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
